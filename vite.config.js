d02db90e649dfa7cdecf3d147b922fc6af3ef7bd
alexklokov <sasha.klokov.2000@gmail.com>
2026-05-30 12:26:12 +0300

    Корректировка конфига vite для gh-pages. Билд для gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash].[ext]',
+      }
+    }
+  }
 })
